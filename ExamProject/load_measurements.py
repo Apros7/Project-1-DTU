8a7ac05e1ed84d7c5f20b90f6732d8e9342dd766
AlecHero <alexander.wittrup@gmail.com>
2022-11-20 15:42:24 +0100

    loadmeasurements

--- ExamProject/load_measurements.py
+++ ExamProject/load_measurements.py
@@ -1,2 +1,16 @@
-def load_measurements(filename, fmode):
-    
+import numpy as np
+import pandas as pd
+import os
+
+
+def load_measurements(filename, fmode=None):
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    path = abspath + "\\" + filename
+
+    data = [row.split(",") for row in open(path, "r")]
+    data = np.array(data,dtype=float)
+
+    tvec = data[:,:6]
+    data = data[:,6:]
+
+    return tvec, data
