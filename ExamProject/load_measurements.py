a94a8ae3fe2c142af5a8c7f77e5119a14faec7bb
apros7 <lucas.vilsen@gmail.com>
2022-11-20 16:20:45 +0100

    new functions

--- ExamProject/load_measurements.py
+++ ExamProject/load_measurements.py
@@ -5,7 +5,7 @@ import os
 
 def load_measurements(filename, fmode=None):
     abspath = os.path.dirname(os.path.abspath(__file__))
-    path = abspath + "\\" + filename
+    path = abspath + "/" + filename
 
     data = [row.split(",") for row in open(path, "r")]
     data = np.array(data,dtype=float)
