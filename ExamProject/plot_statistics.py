efcbb32395ebda32937d6a4eeba7711f927bc63f
apros7 <lucas.vilsen@gmail.com>
2022-11-21 11:46:35 +0100

    lol

--- ExamProject/plot_statistics.py
+++ ExamProject/plot_statistics.py
@@ -1,6 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_statistics(tvec, data, zone="All"):
+from load_measurements import load_measurements
+tvec, data = load_measurements("testdata3.csv")
+
+
+# Tvec skal kun indeholde relevante tidsdata
+tvec = tvec[:, 4]
+
+def plot_statistics(tvec, data, zone="All", time="minutes"):
+    title = "all zones"
     if zone != "All":
-        data = data[zone-1, :]
-    print(data)
-    print(len(data))
+        data = data[:, zone-1]
+        title = f"zone {zone}"
+
+    # Calculate size:
+    size = 1
+    for dim in np.shape(data): 
+        size *= dim
+
+    number_of_zones = 1
+    if title == "all zones":
+        number_of_zones = 4
+    for i in range(number_of_zones):
+        if size < 25:
+            plt.bar(tvec, data)
+        else:
+            plt.plot(data)
+    plt.title(f"Consuption for {title} per {time}")
+    plt.xlabel(f"Time in {time}")
+    plt.ylabel("Energy used in Watt-hours")
+    plt.show()
+
+
+    
+plot_statistics(tvec, data, zone=1)
